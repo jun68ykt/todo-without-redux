ef822a9d02f50a34785ae6bf2851c6ebde72fb65
jun68ykt <jun68ykt@teratail>
2019-09-11 22:16:38 +0900

    AddTodo をreduxとconnectしないよう修正

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { addTodo } from '../actions'
 
-const AddTodo = ({ dispatch }) => {
+const AddTodo = () => {
   let input
 
   return (
@@ -13,7 +11,7 @@ const AddTodo = ({ dispatch }) => {
           if (!input.value.trim()) {
             return
           }
-          dispatch(addTodo(input.value))
+          // TODO: addTodo の処理をあとで追加
           input.value = ''
         }}
       >
@@ -24,4 +22,4 @@ const AddTodo = ({ dispatch }) => {
   )
 }
 
-export default connect()(AddTodo)
+export default AddTodo
