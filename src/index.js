5b4cf965d0db5ac14c10904198f71fad30c23b56
jun68ykt <jun68ykt@teratail>
2019-09-12 00:06:23 +0900

    Redux関連のコードを除去

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers'
 import App from './containers/App'
 
-const store = createStore(rootReducer)
-
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 )
