30d8aceea703561019951cddc8a8f48a0736580d
jun68ykt <jun68ykt@teratail>
2019-09-11 22:47:27 +0900

    ReduxとconnectするVisibilityTodoListは不要のため削除

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import TodoList from '../components/TodoList'
 
 class App extends React.Component {
   render () {
     return (
       <div>
         <AddTodo />
-        <VisibleTodoList />
+        {/* TODO: あとで todos と toggleTodo を修正 */}
+        <TodoList todos={[]} toggleTodo={() => {}} />
         <Footer />
       </div>
     )
